e68554d07f6caeb4a04a148b4ac341fb99b0980d
e <liujia@qq.com>
2020-06-02 11:22:30 +0800

    chapter3-commit

---
+++ untitled2/src/com/concurrent/chapter1/TicketWIndowRunnable.java
@@ -0,0 +1,29 @@
+package com.concurrent.chapter1;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+public class TicketWIndowRunnable implements Runnable {
+    private static final int MAX = 50;
+    private int index;
+
+    @Override
+    public void run() {
+        System.out.println("hello world this is update");
+        while (index<MAX){
+            System.out.println(Thread.currentThread()+" 的号码是："+index++);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("hello git this is update for 模板方法");
+
+    }
+
+    public static void main(String[] args) {
+        TicketWIndowRunnable ticketWIndowRunnable = new TicketWIndowRunnable();
+        IntStream.range(1,4).forEach(i->{new Thread(ticketWIndowRunnable,"sellwindow"+i).start();});
+    }
+}
